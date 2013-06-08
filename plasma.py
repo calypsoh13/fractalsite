5556222c92e917b1c0636226b6f47ae9daeb27df
calypsoh13 <calypsoh13@aol.com>
2013-06-08 00:45:10 -0500

    perturbance and center displacement
    Added perturbance parameter.
    Added center displacement methods.

--- plasma.py
+++ plasma.py
@@ -3,49 +3,38 @@ import png
 
 didCancel = False
 
-# plasma function
+# plasma function using the diamond square algorithm
 # returns a square matrix of values between 0 and 1
-# for the given size, roughness (from 0 to 1), and a list of initial values.
-# Note: The algoritm only works for sizes = 2^n+1 (9, 17, 33, 65, etc.)
-# other sizes will give results with unwanted vertical and horizontal features. 
-def diamondSquareFractal(size, roughness, cornerValues = None):
-    
-    #print "size = ", size, "roughness =", roughness, "corner values =", cornerValues
+# size: the length and width of the square matrix. 
+# For best results, the size should be = 2^n+1 where n is and integer.
+# (9, 17, 33, 65, etc).  Other sizes will give results with unwanted 
+# vertical and horizontal features. 
+# roughness (from 0 to 1): the overall noise level 
+# perturbance (from 0 to 1): the size-proportional noise level
+# cornerValues: a list of initial values.  If the list contains 1 value, it's used
+# for all four corners.  If it contains 2 values, the first is used for tl and br and 
+# second is used for the other corners.  If it contains 4 values, they are placed at
+# tl, tr, bl and br.
+# edgeError: whether to apply the noise level to the edges of the fractal squares.
+# midError: whether to apply the noise level to the midpoint of the fractal squares. 
+# (Either edgeError or midError should be true to produce some variability.)
+
+#use numpy.savetxt("matrix.txt", matrix) to save
+
+def diamondSquareFractal(size, roughness = .5, perturbance = .5,\
+                         cornerValues = None, edgeError = True, midError = True):
 
     matrix = zeros((size, size))
     
-    if None == cornerValues:
-        pass
-        matrix[0, 0] = random.random()
-        matrix[0, size-1] = random.random()
-        matrix[size-1, 0] = random.random()
-        matrix[size-1, size-1] = random.random()
+    if None == cornerValues :
+        corners = []
+    elif isinstance(cornerValues, list) :
+        corners = cornerValues
     else:
-        try:
-            #allow the user to pass in 4, 2, or 1 value for the corners
-            if len(cornerValues) > 3:
-                matrix[0, 0] = cornerValues[0]
-                matrix[0, size-1] = cornerValues[1]
-                matrix[size-1, 0] = cornerValues[2]
-                matrix[size-1, size-1] = cornerValues[3]
-            elif len(cornerValues) > 1:
-                matrix[0, 0] = cornerValues[0]
-                matrix[0, size-1] = cornerValues[1]
-                matrix[size-1, 0] = cornerValues[1]
-                matrix[size-1, size-1] = cornerValues[0]
-            elif len(cornerValues) == 1:
-                matrix[0, 0] = cornerValues[0]
-                matrix[0, size-1] = cornerValues[0]
-                matrix[size-1, 0] = cornerValues[0]
-                matrix[size-1, size-1] = cornerValues[0]
-        except TypeError:
-            matrix[0, 0] = cornerValues
-            matrix[0, size-1] = cornerValues
-            matrix[size-1, 0] = cornerValues
-            matrix[size-1, size-1] = cornerValues
-
-    #print matrix[0, 0], matrix[0, size-1], matrix[size-1, 0],
-
+        corners = [cornerValues]
+    
+    applyCornerValues(matrix, corners, roughness)
+    
     from collections import deque
     queue = deque()
     queue.append([0, size-1, 0 ,size-1])
@@ -55,7 +44,8 @@ def diamondSquareFractal(size, roughness, cornerValues = None):
         [row, maxRow, col, maxCol] = queue.popleft()
 
         
-        [midRow, midCol] = populate(matrix, row, maxRow, col, maxCol, roughness)
+        [midRow, midCol] = diamondSquarePopulate(matrix, row, maxRow, col, maxCol,\
+             roughness, perturbance, midError, edgeError)
 
         if midRow - row >= 2 or midCol - col >= 2:
                 #add top left square
@@ -79,16 +69,23 @@ def diamondSquareFractal(size, roughness, cornerValues = None):
 
     return matrix
 
-# populate square 
-def populate(matrix, row, maxRow, col, maxCol, roughness):
+# diamondSquarePopulate inner method to populate a square 
+def diamondSquarePopulate(matrix, row, maxRow, col, maxCol, roughness, perturbance,\
+                          midError, edgeError):
     
     rowRange = maxRow - row + 1
     colRange = maxCol - col + 1
     
-    #print "*** populate:", row, maxRow, rowRange, col, maxCol, colRange
+    #print "*** diamondSquarePopulate:", row, maxRow, rowRange, col, maxCol, colRange
     
-    sizeFactor = math.log(rowRange, 2)/ math.log(matrix.shape[0],2)
-    noiseLevel = roughness * sizeFactor
+    pf = perturbanceFactor(matrix.shape[0], rowRange, perturbance)
+    noiseLevel = roughness * pf
+
+    midNoise = 0
+    edgeNoise = 0
+    
+    if (midError == True): midNoise = noiseLevel
+    if (edgeError == True): edgeNoise = noiseLevel
     
     shape = matrix.shape
     
@@ -97,7 +94,7 @@ def populate(matrix, row, maxRow, col, maxCol, roughness):
     
     # do diamond step
     
-    diamondValue = getValue(noiseLevel, [matrix[row, col], matrix[row, maxCol], \
+    diamondValue = getValue(midNoise, [matrix[row, col], matrix[row, maxCol], \
                                     matrix[maxRow, col], matrix[maxRow, maxCol]])
     
     if (midRow > row and midRow < maxRow) and (midCol > col and midCol < maxCol):
@@ -115,13 +112,14 @@ def populate(matrix, row, maxRow, col, maxCol, roughness):
         values = [matrix[row, col], matrix[maxRow, col], diamondValue]
         if leftMost >= 0:
             values.append(matrix[midRow, leftMost])
-        matrix[midRow, col] = getValue(noiseLevel, values)
+        matrix[midRow, col] = getValue(edgeNoise, values)
         #print "setting left", midRow, col
         
         # only do the right side if we are on the right edge
         if maxCol == shape[0] - 1:
-            matrix[midRow, maxCol] =  getValue(noiseLevel,\
-                                               [matrix[row, maxCol], matrix[maxRow, maxCol], \
+            matrix[midRow, maxCol] =  getValue(edgeNoise,\
+                                               [matrix[row, maxCol],\
+                                                matrix[maxRow, maxCol], \
                                                 diamondValue])
             #print "setting right", midRow, maxCol
     
@@ -131,21 +129,145 @@ def populate(matrix, row, maxRow, col, maxCol, roughness):
         values = [matrix[row, col], matrix[row, maxCol], diamondValue]
         if topMost >= 0:
             values.append(matrix[topMost, midCol])
-        matrix[row, midCol] = getValue(noiseLevel, values)
+        matrix[row, midCol] = getValue(edgeNoise, values)
         #print "setting top", row, midCol
         
         # only do the bottom side if we are on the bottom edge
         if maxRow == shape[1] - 1:
-            matrix[maxRow, midCol] = getValue(noiseLevel, \
-                                              [matrix[maxRow, col], matrix[maxRow, maxCol], \
-                                               diamondValue])
+            matrix[maxRow, midCol] = getValue(edgeNoise, \
+                                    [matrix[maxRow, col],\
+                                    matrix[maxRow, maxCol], \
+                                    diamondValue])
             #print "setting bottom", maxRow, midCol
             
         #printAllowCancel(matrix)
     return [midRow, midCol]
 
-def getValue(noiseLevel, values):
+# plasma function using the midpoint displacement algorithm
+def midpointDisplacementFractal(size, roughness = .5, perturbance = .5,\
+                         cornerValues = None, edgeError = True, midError = True):
+                         
+   #print "size = ", size, "roughness =", roughness, "corner values =", cornerValues
+
+    matrix = zeros((size, size))
     
+    if None == cornerValues:
+        corners = []
+    elif isinstance(cornerValues, list):
+        corners = cornerValues
+    else:
+        corners = [cornerValues]
+    
+    applyCornerValues(matrix, corners, roughness)
+    
+    midpointDisplacementPopulate(matrix, 0, size-1, 0, size-1, roughness, perturbance, 
+                                 edgeError, midError)
+    
+    return matrix
+    
+def midpointDisplacementPopulate(matrix, row, maxRow, col, maxCol, 
+                                 roughness, perturbance,\
+                                 edgeError, midError):
+    
+    rowRange = maxRow - row + 1
+    colRange = maxCol - col + 1
+
+    if rowRange <= 2 and colRange <= 2: return
+           
+    shape = matrix.shape
+    
+    pf = perturbanceFactor(shape[0], rowRange, perturbance)
+    noiseLevel = roughness * pf
+    
+    midNoise = 0
+    edgeNoise = 0
+    
+    if (midError == True): midNoise = noiseLevel
+    if (edgeError == True): edgeNoise = noiseLevel
+    
+    midRow = row + int(rowRange / 2)
+    midCol = col + int(colRange / 2)
+    
+    # check whether there's an empty row
+    if midRow > row and midRow < maxRow:
+        # only do the left side if we are on the left edge
+        if col == 0 :
+            matrix[midRow, col] =\
+                getValue(edgeNoise, [matrix[row, col], matrix[maxRow, col]])
+            #print "setting left", midRow, col
+        
+        # always do the right side
+        matrix[midRow, maxCol] =\
+            getValue(edgeNoise, [matrix[row, maxCol], matrix[maxRow, maxCol]])
+        #print "setting right", midRow, maxCol
+    
+    # check whether there's an empty column
+    if midCol > col and midCol < maxCol:
+        # only do the top if we are at the top edge
+        if row == 0 :
+            matrix[row, midCol] =\
+                getValue(edgeNoise, [matrix[row, col], matrix[row, maxCol]])
+            #print "setting top", row, midCol
+        
+        # always do the bottom
+        matrix[maxRow, midCol] =\
+            getValue(edgeNoise, [matrix[maxRow, col], matrix[maxRow, maxCol]])
+
+    
+    if (midRow > row) and (midRow < maxRow) and (midCol > col) and (midCol < maxCol):
+        values = [matrix[row, col], matrix[row, maxCol], \
+                  matrix[maxRow, col], matrix[maxRow, maxCol]]
+        matrix[midRow, midCol] = getValue(midNoise, values)
+            
+    #printAllowCancel(matrix)
+    
+    midpointDisplacementPopulate(matrix, row, midRow, col, midCol, \
+                                 roughness, perturbance, 
+                                 edgeError, midError)
+    midpointDisplacementPopulate(matrix, row, midRow, midCol, maxCol, \
+                                 roughness, perturbance, 
+                                 edgeError, midError)
+    midpointDisplacementPopulate(matrix, midRow, maxRow, col, midCol, \
+                                 roughness, perturbance,
+                                 edgeError, midError)
+    midpointDisplacementPopulate(matrix, midRow, maxRow, midCol, maxCol, \
+                                 roughness, perturbance,
+                                 edgeError, midError)
+             
+def applyCornerValues(matrix, cornerValues, roughness):
+
+    size = matrix.shape[0]
+    
+    #allow the user to pass in 4, 2, or 1 value for the corners
+    if len(cornerValues) > 3:
+        matrix[0, 0] = cornerValues[0]
+        matrix[0, size-1] = cornerValues[1]
+        matrix[size-1, 0] = cornerValues[2]
+        matrix[size-1, size-1] = cornerValues[3]
+    elif len(cornerValues) > 1:
+        matrix[0, 0] = cornerValues[0]
+        matrix[0, size-1] = cornerValues[1]
+        matrix[size-1, 0] = cornerValues[1]
+        matrix[size-1, size-1] = cornerValues[0]
+    elif len(cornerValues) == 1:
+        matrix[0, 0] = cornerValues[0]
+        matrix[0, size-1] = cornerValues[0]
+        matrix[size-1, 0] = cornerValues[0]
+        matrix[size-1, size-1] = cornerValues[0]
+    else:
+        matrix[0, 0] = random.random() * roughness
+        matrix[0, size-1] = random.random() * roughness
+        matrix[size-1, 0] = random.random() * roughness
+        matrix[size-1, size-1] = random.random() * roughness
+
+
+def perturbanceFactor(lenWhole, lenPart, perturbance):
+    k = 1 - perturbance
+    return lenPart ** k / lenWhole ** k
+    
+
+def getValue(noiseLevel, values):
+        
     randomValue = random.random()
     
     averageValue = sum(values)/len(values)
@@ -161,6 +283,6 @@ def printAllowCancel(matrix):
         
     response = raw_input('ctl_c to stop >')
 
-#use numpy.savetxt("matrix.txt", matrix) to save
+
     
 
