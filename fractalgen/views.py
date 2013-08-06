98af380897966c12d1bf43c9e6d3252bc54ceb7b
Kat Harrison <katherinej.harrison@gmail.com>
2013-08-06 03:17:13 -0400

    edit django files to render index.html properly

--- fractalgen/views.py
+++ fractalgen/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.core.context_processors import csrf
 
 def create(request):
-    return render_to_response('create_fractal.html')
+    return render_to_response('index.html')
 
