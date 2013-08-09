b6a1fae257c10eeeaff6d0638f3ba67d33b6b45e
Kat Harrison <katherinej.harrison@gmail.com>
2013-08-09 15:07:49 -0400

    Update models to be consistent with new design.

--- fractalgen/models.py
+++ fractalgen/models.py
@@ -1,25 +1,39 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.core.validators import MaxValueValidator, MinValueValidator
-from time import time
 
-def get_upload_filename(instance, filename):
-    f = filename.split('/')[-1]
-    return "images/uploaded_files/%s" % (f)
-            
+class RawFractal(models.Model):
+    author = models.ForeignKey(User)
+    rawfractimg = models.CharField(max_length=40)
+    size = models.IntegerField(default=0)
+    roughness = models.DecimalField(max_digits=2, decimal_places=1)
+    perturbance = models.DecimalField(max_digits=2, decimal_places=1)
+
+
 class Fractal(models.Model):
-    # Fields entered programatically
     author = models.ForeignKey(User)
     pub_date = models.DateTimeField('date created', auto_now=True)
-    fractalimg = models.FileField(upload_to=get_upload_filename)
-    # Fields entered in form by user
+    fractalimg = models.CharField(max_length=40)
     title = models.CharField(max_length=40)
-    # Fields updated later by other users
     likes = models.IntegerField(default=0)
-    
+    rawfractal = models.ForeignKey(RawFractal)
+    useheat = models.BooleanField()
+
     def __unicode__(self):
         return self.title
 
+
+class Filter(models.Model):
+    fractal = models.ForeignKey(Fractal)
+    X1 = models.IntegerField(default=0)
+    Y1 = models.IntegerField(default=0)
+    sigX1 = models.DecimalField(max_digits=3, decimal_places=2)
+    sigY1 = models.DecimalField(max_digits=3, decimal_places=2)
+
+class ColorStop(models.Model):
+    fractal = models.ForeignKey(Fractal)
+    color = models.CharField(max_length=8)
+    stop = models.IntegerField(default=0)
+    
 class Comment(models.Model):
     author = models.ForeignKey(User)
     body = models.TextField()
