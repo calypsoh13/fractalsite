779eded168787f7f7a3d24e9226edaad777248e1
Kat Harrison <katherinej.harrison@gmail.com>
2013-08-08 22:14:29 -0400

    Basic require.js config with shim also initial app function.

--- static/js/main.js
+++ static/js/main.js
@@ -1,23 +1,43 @@
-require.config({
-  paths: {
-    jquery: 'lib/jquery.min',
-    lodash: 'lib/lodash.min',
-    backbone: 'lib/backbone',
-    underscore: 'lib/underscore',
-    base: 'base',
-    backbone-localStorage: 'lib/backbone.localStorage',
-    
-    
-  }
+'use strict';
 
+require.config({
+    shim: {
+        underscore: {
+            exports: '_'
+        },
+        backbone: {
+            deps: [
+                'underscore',
+                'jquery'
+            ],
+            exports: 'Backbone'
+        },
+        backboneLocalstorage: {
+            deps: ['backbone'],
+            exports: 'Store'
+        },
+        spectrum: {
+            deps: ['jquery'],
+            exports: 'spectrum',
+        }
+    },
+    paths: {
+        jquery: 'lib/jquery.min',
+        underscore: 'lib/lodash.min',
+        backbone: 'lib/backbone',
+        backboneLocalStorage: 'lib/backbone.localStorage',
+        spectrum: 'spectrum/spectrum' 
+    }
 });
 
 require([
-  // Load our app module and pass it to our definition function
-  'app',
-
-], function(App){
-  // The "app" dependency is passed in as "App"
-  // Again, the other dependencies passed in are not "AMD" therefore don't pass a parameter to this function
-  App.initialize();
+    'backbone',
+    'app',
+    'views/createview',
+    'router'
+], function(Backbone, App, CreateView, Workspace){
+    new Workspace();
+    Backbone.history.start();
+    App.initialize();
+    new CreateView();
 });
