cf3860a914f6c030a0b96bb325022a7bf3e79f22
Kat Harrison <katherinej.harrison@gmail.com>
2013-08-18 14:52:05 -0400

    Set up api
    Currently, POST from backbone fails with 404.

--- fractalgen/api.py
+++ fractalgen/api.py
@@ -1,7 +1,23 @@
 from tastypie.resources import ModelResource
-from fractalgen.models import Fractal
+from fractalgen.models import RawFractal
+from tastypie.authorization import Authorization
+from django.contrib.auth.models import User
+from tastypie import fields
 
-class FractalResource(ModelResource):
+class UserResource(ModelResource):
     class Meta:
-        queryset = Fractal.objects.all()
-        resource_name = 'fractal'
+        queryset = User.objects.all()
+        resource_name = 'user'
+        default_format = 'application/json'
+
+class RawFractalResource(ModelResource):
+    user = fields.ForeignKey(UserResource, 'user')
+
+    class Meta:
+        queryset = RawFractal.objects.all()
+        resource_name = 'rawfractal'
+        default_format = 'application/json'
+        authorization = Authorization()
+        list_allowed_methods = ['get', 'post']
+        detail_allowed_methods = ['get']
+        always_return_data = True
--- fractalgen/urls.py
+++ fractalgen/urls.py
@@ -1,12 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-        url(r'^$', 'fractalgen.views.fractals'),
-        url(r'^(?P<fractal_id>\d+)/$', 'fractalgen.views.fractal'),
         url(r'^create/$', 'fractalgen.views.create'),
-        url(r'^save/$', 'fractalgen.views.save'),
-        url(r'^like/(?P<fractal_id>\d+)/$', 'fractalgen.views.like_fractal'),
-        url(r'^add_comment/(?P<fractal_id>\d+)/$', 'fractalgen.views.add_comment'),
 
         )
 
